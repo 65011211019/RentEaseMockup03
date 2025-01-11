0e572fe91d1db446870501a9dc1e851d3e15f2e2
65011211019 <138645306+65011211019@users.noreply.github.com>
2025-01-12 01:39:24 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'gradient-start': '#B657FF', // Light Purple for gradient start
-        'gradient-end': '#FF995C', // Light Orange for gradient end
-        'button-pink': '#D97BFF', // Pink for buttons
-        'button-orange': '#FF995C', // Orange for buttons
         'dark-bg': '#100617', // Dark background
-        'pink-to-black-start': '#FF4B8A', // Custom pink color for gradient start
-        'pink-to-black-end': '#000000', // Black color for gradient end
       }
     },
   },
